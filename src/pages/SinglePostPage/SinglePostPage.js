dbef1da80b721a74791a3a8b6e31931209bd4931
MarkVed17 <vedantlahane17@gmail.com>
2022-09-17 01:57:35 +0530

    style: applied styling to `Single Post Page`

--- src/pages/SinglePostPage/SinglePostPage.js
+++ src/pages/SinglePostPage/SinglePostPage.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { usePosts } from "../../contexts";
+import "./SinglePostPage.css";
 
 const SinglePostPage = () => {
   const [post, setPost] = useState();
@@ -15,9 +16,31 @@ const SinglePostPage = () => {
   return (
     <>
       {post && (
-        <div>
-          <h1>{post.title}</h1>
+        <div className="single-post">
+          <h2 className="single-post-title">{post.title}</h2>
+          <h5 className="single-post-author">Posted by {post.author}</h5>
           <img src={post.thumbnail} alt={post.title} />
+
+          <div className="single-post-stats">
+            <span className="single-post-stats-icon">
+              <span>{post.ups}</span>
+              <span class="material-symbols-outlined">arrow_upward</span>
+            </span>
+
+            <span className="single-post-stats-icon">
+              <span>{post.downs}</span>
+              <span class="material-symbols-outlined">arrow_downward</span>
+            </span>
+
+            <span className="single-post-stats-icon">
+              <span>{post.num_comments}</span>
+              <span class="material-symbols-outlined">chat_bubble</span>
+            </span>
+
+            <span className="single-post-stats-icon">
+              <span class="material-symbols-outlined">google_plus_reshare</span>
+            </span>
+          </div>
         </div>
       )}
     </>
