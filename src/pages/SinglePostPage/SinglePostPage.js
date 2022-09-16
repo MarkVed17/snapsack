b93b8ff5c10bf53b7faa4bb710a03aa51ce017a9
MarkVed17 <vedantlahane17@gmail.com>
2022-09-17 02:36:47 +0530

    fix: added placeholder thumbnail

--- src/pages/SinglePostPage/SinglePostPage.js
+++ src/pages/SinglePostPage/SinglePostPage.js
@@ -19,7 +19,15 @@ const SinglePostPage = () => {
         <div className="single-post">
           <h2 className="single-post-title">{post.title}</h2>
           <h5 className="single-post-author">Posted by {post.author}</h5>
-          <img src={post.thumbnail} alt={post.title} />
+          <img
+            src={post.thumbnail}
+            alt={post.title}
+            onError={({ currentTarget }) => {
+              currentTarget.onerror = null; // prevents looping
+              currentTarget.src =
+                "https://media.istockphoto.com/vectors/thumbnail-image-vector-graphic-vector-id1147544807?k=20&m=1147544807&s=612x612&w=0&h=pBhz1dkwsCMq37Udtp9sfxbjaMl27JUapoyYpQm0anc=";
+            }}
+          />
 
           <div className="single-post-stats">
             <span className="single-post-stats-icon">
