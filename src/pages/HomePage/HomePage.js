13c0d512e3dea0e64f72ff2a6ef5200dfe25816e
MarkVed17 <vedantlahane17@gmail.com>
2022-09-16 17:44:03 +0530

    feat: created a raw `Home Page`

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,5 +1,28 @@
+import "./HomePage.css";
+import { usePosts } from "../../contexts";
+import { useNavigate } from "react-router-dom";
+
 const HomePage = () => {
-  return <>Home Page</>;
+  const { posts } = usePosts();
+  const navigate = useNavigate();
+
+  return (
+    <div className="posts-container">
+      {posts &&
+        posts.map(({ data: { title, thumbnail, name } }) => (
+          <div
+            key={name}
+            className="post"
+            onClick={() => {
+              navigate(`/post/${name}`);
+            }}
+          >
+            <h1>{title}</h1>
+            <img src={thumbnail} alt={title} />
+          </div>
+        ))}
+    </div>
+  );
 };
 
 export { HomePage };
