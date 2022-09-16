6f26f7ab73d5a2214b11a2f9479a4a4abd346a97
MarkVed17 <vedantlahane17@gmail.com>
2022-09-17 01:58:34 +0530

    feat: created `Filters Container` component

---
+++ src/components/FiltersContainer/FiltersContainer.js
@@ -0,0 +1,38 @@
+import "./FiltersContainer.css";
+import { useFilter } from "../../contexts";
+
+const FiltersContainer = () => {
+  const {filterBy, setFilterBy } = useFilter();
+
+  const filterHandler = (e) => {
+    if (filterBy === e.currentTarget.childNodes[0].textContent) {
+      console.log(e.currentTarget.childNodes[0].textContent);
+      setFilterBy("Best");
+    } else {
+      setFilterBy(e.currentTarget.childNodes[0].textContent);
+    }
+  };
+
+  return (
+    <div className="filters-container">
+      <button className="filter-chip" onClick={filterHandler}>
+        Best
+        <span class="material-symbols-outlined">rocket</span>
+      </button>
+      <button className="filter-chip" onClick={filterHandler}>
+        Hot
+        <span class="material-symbols-outlined">local_fire_department</span>
+      </button>
+      <button className="filter-chip" onClick={filterHandler}>
+        New
+        <span class="material-symbols-outlined">new_releases</span>
+      </button>
+      <button className="filter-chip" onClick={filterHandler}>
+        Top
+        <span class="material-symbols-outlined">leaderboard</span>
+      </button>
+    </div>
+  );
+};
+
+export { FiltersContainer };
