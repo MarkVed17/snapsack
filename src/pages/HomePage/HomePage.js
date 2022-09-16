e3b2204eb65d2105351eb7b02f5d98baee0ae6dd
MarkVed17 <vedantlahane17@gmail.com>
2022-09-16 19:01:30 +0530

    feat: added raw `Filters` component

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,26 +1,43 @@
 import "./HomePage.css";
-import { usePosts } from "../../contexts";
+import { usePosts, useFilter } from "../../contexts";
 import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
   const { posts } = usePosts();
+  const { filteredPosts, filterBy, setFilterBy } = useFilter();
   const navigate = useNavigate();
 
+  const filterHandler = (e) => {
+    if (filterBy === e.currentTarget.textContent) {
+      setFilterBy("Best");
+    } else {
+      setFilterBy(e.currentTarget.textContent);
+    }
+  };
+
   return (
-    <div className="posts-container">
-      {posts &&
-        posts.map(({ data: { title, thumbnail, name } }) => (
-          <div
-            key={name}
-            className="post"
-            onClick={() => {
-              navigate(`/post/${name}`);
-            }}
-          >
-            <h1>{title}</h1>
-            <img src={thumbnail} alt={title} />
-          </div>
-        ))}
+    <div>
+      <div className="filters-container">
+        <button onClick={filterHandler}>Best</button>
+        <button onClick={filterHandler}>Hot</button>
+        <button onClick={filterHandler}>New</button>
+        <button onClick={filterHandler}>Top</button>
+      </div>
+      <div className="posts-container">
+        {posts &&
+          filteredPosts.map(({ data: { title, thumbnail, name } }) => (
+            <div
+              key={name}
+              className="post"
+              onClick={() => {
+                navigate(`/post/${name}`);
+              }}
+            >
+              <h1>{title}</h1>
+              <img src={thumbnail} alt={title} />
+            </div>
+          ))}
+      </div>
     </div>
   );
 };
