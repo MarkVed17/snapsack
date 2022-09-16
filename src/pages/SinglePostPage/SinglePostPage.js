d243c41d0c925076a08b3e3bad4b1e07884e6956
MarkVed17 <vedantlahane17@gmail.com>
2022-09-16 17:46:12 +0530

    feat: created a raw `Single Post Page`

--- src/pages/SinglePostPage/SinglePostPage.js
+++ src/pages/SinglePostPage/SinglePostPage.js
@@ -1,6 +1,27 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { usePosts } from "../../contexts";
+
 const SinglePostPage = () => {
-    return <>Single Post Page</>;
-  };
-  
-  export { SinglePostPage };
-  
+  const [post, setPost] = useState();
+  const { posts } = usePosts();
+  const { postname } = useParams();
+
+  useEffect(() => {
+    const findPost = posts.find((post) => post.data.name === postname);
+    setPost(findPost.data);
+  }, [posts, postname]);
+
+  return (
+    <>
+      {post && (
+        <div>
+          <h1>{post.title}</h1>
+          <img src={post.thumbnail} alt={post.title} />
+        </div>
+      )}
+    </>
+  );
+};
+
+export { SinglePostPage };
