be07d20eb491645ab5dbf6b0c5507723de8503e7
MarkVed17 <vedantlahane17@gmail.com>
2022-09-16 17:43:32 +0530

    provided `posts` context to the `App`

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { PostsProvider } from "./contexts";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <PostsProvider>
+        <App />
+      </PostsProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
